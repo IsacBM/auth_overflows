f9fc25f55d165b520e4aa62a78d36e3d055d505e
IsacBM <isacbritomatos32@gmail.com>
2025-12-03 11:14:07 -0300

    Add CSRF and CORS settings for trusted origins and regex patterns

--- core/settings.py
+++ core/settings.py
@@ -185,3 +185,13 @@ CORS_ALLOWED_ORIGINS = [
     "ionic://localhost",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+    "https://localhost",
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://localhost(:\d+)?$",
+    r"^http://localhost(:\d+)?$",
+    r"^capacitor://localhost$",
+]
